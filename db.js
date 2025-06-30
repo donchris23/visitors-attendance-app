be4822a41556bf21f0821e8b8f9fa9eeda87f394
CHRISTIAN ONYEKACHI NJOKU <CNJOKU@may-baker.local>
2025-06-30 12:38:58 +0100

    new2

--- db.js
+++ db.js
@@ -1,38 +1,56 @@
+// require('dotenv').config();
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST || 'metro.proxy.rlwy.net',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || 'sIQqQeXGpMnCjulennZMruyDOvLvfsIh',
+//   database: process.env.DB_NAME || 'railway',
+//   port: process.env.DB_PORT || 24535
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     throw err;
+//   }
+//   console.log('MySQL Connected');
+// });
+
+// module.exports = db;
+
+// const connection = mysql.createConnection({
+//   host: 'metro.proxy.rlwy.net',
+//   user: 'root',
+//   password: 'sIQqQeXGpMnCjulennZMruyDOvLvfsIh',
+//   database: 'railway',
+//   port: 24535,
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     throw err;
+//   }
+//   console.log('Connected to Railway MySQL database');
+// });
+
+// module.exports = connection;
+
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+// Create a MySQL connection pool with promise support
+const pool = mysql.createPool({
   host: process.env.DB_HOST || 'metro.proxy.rlwy.net',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'sIQqQeXGpMnCjulennZMruyDOvLvfsIh',
   database: process.env.DB_NAME || 'railway',
-  port: process.env.DB_PORT || 24535
+  port: process.env.DB_PORT || 24535,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    throw err;
-  }
-  console.log('MySQL Connected');
-});
-
-module.exports = db;
-
-const connection = mysql.createConnection({
-  host: 'metro.proxy.rlwy.net',
-  user: 'root',
-  password: 'sIQqQeXGpMnCjulennZMruyDOvLvfsIh',
-  database: 'railway',
-  port: 24535,
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    throw err;
-  }
-  console.log('Connected to Railway MySQL database');
-});
-
-module.exports = connection;
+// Export the promise pool so you can use async/await in other files
+module.exports = pool.promise();
