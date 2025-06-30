923a6cc18a59897d6e3244b3ad48aee01227f1fa
CHRISTIAN ONYEKACHI NJOKU <CNJOKU@may-baker.local>
2025-06-30 13:16:27 +0100

    new add

--- db.js
+++ db.js
@@ -50,8 +50,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 10000, // 10 seconds
-  acquireTimeout: 10000, // 10 seconds
+  connectTimeout: 10000 // 10 seconds
 });
 
 // Export the promise pool so you can use async/await in other files
