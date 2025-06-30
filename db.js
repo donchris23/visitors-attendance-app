3dd3c329a5474953a9a2c5266b899913bbe8c7b0
CHRISTIAN ONYEKACHI NJOKU <CNJOKU@may-baker.local>
2025-06-30 12:06:49 +0100

    update

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'turntable.proxy.rlwy.net',
+  host: process.env.DB_HOST || 'metro.proxy.rlwy.net',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'ACIWmPfhAdIdiZjlSDaaLCMBZDbRCksA',
+  password: process.env.DB_PASSWORD || 'sIQqQeXGpMnCjulennZMruyDOvLvfsIh',
   database: process.env.DB_NAME || 'railway',
-  port: process.env.DB_PORT || 34817
+  port: process.env.DB_PORT || 24535
 });
 
 db.connect((err) => {
